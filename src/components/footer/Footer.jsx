781815dfa1ed7c11e6adc4e57c72ee8767f374ae
Shweta Kadam <Kadamshweta2108@gmmail.com>
2023-12-27 10:55:02 +0530

    check Out experience button inside the footer

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -16,7 +16,7 @@ const Footer = () => {
       <ul className="permalinks">
       <li><a href="#">HOME</a></li>
       <li><a href="#about">About</a></li>
-      <li><a href="#expeirience">Expeirience</a></li>
+      <li><a href="#experience">Experience</a></li>
       <li><a href="#services">Services</a></li>
       <li><a href="#portfolio">Portfolio</a></li>
       <li><a href="#testimonials">Testmonials</a></li>
